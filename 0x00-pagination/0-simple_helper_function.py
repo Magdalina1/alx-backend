97416dbca25c6821f95117601d52d14dec11cdcf
Magdalina1 <majdalol95@gmail.com>
2024-05-06 11:52:50 +0000

    0-simple_helper_function.py

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Write a function named index_range that takes two integer
+arguments page and page_size."""
+
+
+from typing import tuple
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Start index and an end index corresponding to the range of"""
+    return ((page-1) * page_size, page_size * page)
