21fdc11657bed311aa1283642b5364f605295728
Magdalina1 <majdalol95@gmail.com>
2024-05-08 13:17:53 +0000

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from BaseCaching and is a caching system."""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """_summary_"""
+
+    def __init__(self):
+        """_summary_"""
+        super().__init__()
+
+    def put(self, key, item):
+        """_summay_"""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key."""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
