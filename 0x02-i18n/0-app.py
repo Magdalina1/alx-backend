36f3f05f914e54ae8c9cae13baee64ab641b3c08
Magdalina1 <majdalol95@gmail.com>
2024-05-14 17:20:02 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_tamplete
+from flask import Flask, render_templete
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_tamplete("0-index.html")
+    return render_templete("0-index.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
