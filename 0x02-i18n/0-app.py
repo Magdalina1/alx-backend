ed21a53bd76c774ce657a51b11512353e0ad2808
Magdalina1 <majdalol95@gmail.com>
2024-05-14 17:26:29 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,4 +9,4 @@ def hello_world():
     return render_templete("0-index.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
